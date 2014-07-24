aaecd74e2afedcbfb499adc9a4826479415a4a3b
Jens Eraßmy <jens.erassmy@artcom.de>
2014-07-24 13:33:50 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
   Model: {
-    DownloadCollection: require('./model/download-collection.js');
-    DownloadModel: require('./model/download-model.js');
+    DownloadCollection: require('./model/download-collection.js'),
+    DownloadModel: require('./model/download-model.js')
   },
   WebSocket: {
-  	CollectionUpdater: require('./websocket/collection-updater.js');
+    CollectionUpdater: require('./websocket/collection-updater.js')
   }
-}
+};
