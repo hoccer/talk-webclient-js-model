76962696983f794af21d51d4821ca1cdc068e4b7
Jens Eraßmy <jens.erassmy@artcom.de>
2014-07-24 13:17:15 +0200

    Set namespaces
    Change 'models' folder to 'model'

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Model: {
+    DownloadCollection: require('./model/download-collection.js');
+    DownloadModel: require('./model/download-model.js');
+  },
+  WebSocket: {
+  	CollectionUpdater: require('./websocket/collection-updater.js');
+  }
+}
