862a3f8df5b86fe882effa6062dcd21a403e8b25
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-25 09:49:24 +0200

    Remove getter function for property

--- model/download-collection.js
+++ model/download-collection.js
@@ -5,14 +5,13 @@ var Backbone = require('backbone');
 var DownloadModel = require('./download-model');
 
 module.exports = Backbone.Collection.extend({
+  model: DownloadModel,
+
   initialize: function(models, options) {
     this.backendUrl = options.backendUrl;
   },
+
   url: function() {
     return this.backendUrl + '/api/downloads';
-  },
-  backendUrl: function() {
-    return this.backendUrl;
-  },
-  model: DownloadModel
+  }
 });
